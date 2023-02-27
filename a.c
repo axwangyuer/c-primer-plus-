616a5bde9e6d9b85d86cb84af9192243287f0df2
axwangyuer <axwangyuer@gmail.com>
2023-02-27 16:11:54 +0800

    提交01章
    编程练习题1

---
+++ a.c
@@ -0,0 +1,12 @@
+﻿#include <stdio.h>
+
+int main(void)
+{
+    int num;
+    float total;
+    printf("How many inches do you input?\n");//remainde users input inch
+    scanf("%d",&num);
+    total = num * 2.54;
+    printf("%d inches equal %f centimeters.\n",num,total);//convert to centimeter
+    return 0;
+}
