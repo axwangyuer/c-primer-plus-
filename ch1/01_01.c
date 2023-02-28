7eac426d6d01d832818e8bd1eb631adbbe60fcf5
axwangyuer <axwangyuer@gmail.com>
2023-02-28 15:19:31 +0800

    chapter1

--- ch1/01_01.c
+++ ch1/01_01.c
@@ -6,6 +6,6 @@ int main(void)
     printf("How many inches do you input?\n");//remainde users input inch
     scanf("%f",&num);
     total = num * 2.54;
-    printf("%f inches equal %f centimeters.\n",num,total);//convert to centimeter
+    printf("%.2f inches equal %.2f centimeters.\n",num,total);//convert to centimeter
     return 0;
 }
